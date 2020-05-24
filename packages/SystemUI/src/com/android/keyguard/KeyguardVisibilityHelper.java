3ae6a5ba09ccf85c4296cb31e4dfe89b93919979
TikkiTikki <radstylemusic@gmail.com>
2020-05-23 23:36:01 -0300

    Add Ambient Customizations [1/2]
    This adds the option to add custom text and a custom image on the ambient screen. This option is still in development, so be patient, the bugs that arise will be resolved.
    Based on OmniROM Edge Lighting and Custom Headers.
    
    [SuperDroidBond: adapt for R + A13 & put it behind notification lights]
    
    Good Work by @/TikkiX2
    
    Change-Id: I8bbda7e908ef4bae892c59fdbcf6032a799b3f5b
    
    fixup! Ambient Customizations
    
    SDB:
    1. Fix Text Animation
    2. Fix Pulse Lights not visible if Ambient Image activated
    3. Catch NPE in KeyguardVisibilityHelper
    4. Misc. fixes for Ambient Text & Images
    
    Change-Id: Icbc66effd362d5e7d4e77d977960778bfc19dc7a
    
    Co-authored-by: TikkiTikki <radstylemusic@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardVisibilityHelper.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardVisibilityHelper.java
@@ -34,6 +34,12 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 import com.google.errorprone.annotations.CompileTimeConstant;
 
+import com.android.systemui.superior.AmbientText;
+import com.android.systemui.superior.AmbientCustomImage;
+
+import com.android.systemui.R;
+
+
 /**
  * Helper class for updating visibility of keyguard views based on keyguard and status bar state.
  * This logic is shared by both the keyguard status view and the keyguard user switcher.
@@ -50,6 +56,9 @@ public class KeyguardVisibilityHelper {
     private boolean mLastOccludedState = false;
     private final AnimationProperties mAnimationProperties = new AnimationProperties();
     private final LogBuffer mLogBuffer;
+    // Ambient Customization
+    private AmbientText mAmbientText;
+    private AmbientCustomImage mAmbientCustomImage;
 
     public KeyguardVisibilityHelper(View view,
             KeyguardStateController keyguardStateController,
@@ -62,6 +71,8 @@ public class KeyguardVisibilityHelper {
         mDozeParameters = dozeParameters;
         mScreenOffAnimationController = screenOffAnimationController;
         mAnimateYPos = animateYPos;
+        mAmbientText = (AmbientText) mView.findViewById(R.id.text_container);
+        mAmbientCustomImage = (AmbientCustomImage) mView.findViewById(R.id.image_container);
         mLogBuffer = logBuffer;
     }
 
@@ -97,14 +108,35 @@ public class KeyguardVisibilityHelper {
                     .setInterpolator(Interpolators.ALPHA_OUT)
                     .withEndAction(
                             mAnimateKeyguardStatusViewGoneEndRunnable);
+            if (mAmbientCustomImage != null) {
+                mAmbientCustomImage.animate()
+                    .alpha(0f)
+                    .setStartDelay(0)
+                    .setDuration(160);
+            }
+            if (mAmbientText != null) {
+                mAmbientText.animate()
+                    .alpha(0f)
+                    .setStartDelay(0)
+                    .setDuration(160);
+            }
             if (keyguardFadingAway) {
                 mView.animate()
                         .setStartDelay(mKeyguardStateController.getKeyguardFadingAwayDelay())
                         .setDuration(mKeyguardStateController.getShortenedFadingAwayDuration())
                         .start();
-                log("goingToFullShade && keyguardFadingAway");
-            } else {
-                log("goingToFullShade && !keyguardFadingAway");
+                if (mAmbientCustomImage != null) {
+                    mAmbientCustomImage.animate()
+                        .setStartDelay(mKeyguardStateController.getKeyguardFadingAwayDelay())
+                        .setDuration(mKeyguardStateController.getShortenedFadingAwayDuration())
+                        .start();
+                }
+                if (mAmbientText != null) {
+                    mAmbientText.animate()
+                        .setStartDelay(mKeyguardStateController.getKeyguardFadingAwayDelay())
+                        .setDuration(mKeyguardStateController.getShortenedFadingAwayDuration())
+                        .start();
+                }
             }
         } else if (oldStatusBarState == StatusBarState.SHADE_LOCKED && statusBarState == KEYGUARD) {
             mView.setVisibility(View.VISIBLE);
@@ -116,7 +148,20 @@ public class KeyguardVisibilityHelper {
                     .setDuration(320)
                     .setInterpolator(Interpolators.ALPHA_IN)
                     .withEndAction(mAnimateKeyguardStatusViewVisibleEndRunnable);
-            log("keyguardFadingAway transition w/ Y Aniamtion");
+            if (mAmbientCustomImage != null) {
+                mAmbientCustomImage.setAlpha(0f);
+                mAmbientCustomImage.animate()
+                    .alpha(1f)
+                    .setStartDelay(0)
+                    .setDuration(320);
+            }
+            if (mAmbientText != null) {
+                mAmbientText.setAlpha(0f);
+                mAmbientText.animate()
+                    .alpha(1f)
+                    .setStartDelay(0)
+                    .setDuration(320);
+            }
         } else if (statusBarState == KEYGUARD) {
             if (keyguardFadingAway) {
                 mKeyguardViewVisibilityAnimating = true;
@@ -142,6 +187,14 @@ public class KeyguardVisibilityHelper {
                     log("keyguardFadingAway transition w/o Y Animation");
                 }
                 animator.start();
+                if (mAmbientCustomImage != null) {
+                    mAmbientCustomImage.animate().alpha(0).setDuration(125)
+                        .setStartDelay(0).start();
+                }
+                if (mAmbientText != null) {
+                    mAmbientText.animate().alpha(0).setDuration(125)
+                        .setStartDelay(0).start();
+                }
             } else if (mScreenOffAnimationController.shouldAnimateInKeyguard()) {
                 log("ScreenOff transition");
                 mKeyguardViewVisibilityAnimating = true;
@@ -153,11 +206,23 @@ public class KeyguardVisibilityHelper {
             } else {
                 log("Direct set Visibility to VISIBLE");
                 mView.setVisibility(View.VISIBLE);
+                    if (mAmbientCustomImage != null) {
+                        mAmbientCustomImage.setAlpha(1f);
+                    }
+                    if (mAmbientText != null) {
+                        mAmbientText.setAlpha(1f);
+                 }
             }
         } else {
             log("Direct set Visibility to GONE");
             mView.setVisibility(View.GONE);
             mView.setAlpha(1f);
+            if (mAmbientCustomImage != null) {
+                mAmbientCustomImage.setAlpha(1f);
+            }
+            if (mAmbientText != null) {
+                mAmbientText.setAlpha(1f);
+            }
         }
 
         mLastOccludedState = isOccluded;
