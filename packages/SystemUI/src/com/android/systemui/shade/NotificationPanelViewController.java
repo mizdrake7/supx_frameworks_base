c5c251269a7a6e393631246b33f0430991d75c8b
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-12 01:53:08 +0200

    NotificationPanelViewController: Only set keyguard indication visibility
    Instead of the whole bottom area
    For AOD content toggle for edge lightning

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3331,6 +3331,15 @@ public final class NotificationPanelViewController implements Dumpable {
         updateKeyguardStatusViewAlignment(/* animate= */ true);
     }
 
+    private void showAodContent(boolean show) {
+        if (DEBUG_PULSE_LIGHT) {
+            Log.d(TAG, "showAodContent show = " + show);
+        }
+        mKeyguardStatusViewController.setViewVisible(show);
+        mKeyguardStatusBar.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
+        mKeyguardIndicationController.setVisible(show);
+    }
+
     public void setAmbientIndicationTop(int ambientIndicationTop, boolean ambientTextVisible) {
         int ambientIndicationBottomPadding = 0;
         if (ambientTextVisible) {
@@ -3342,15 +3351,6 @@ public final class NotificationPanelViewController implements Dumpable {
             updateMaxDisplayedNotifications(true);
         }
     }
-
-    private void showAodContent(boolean show) {
-        if (DEBUG_PULSE_LIGHT) {
-            Log.d(TAG, "showAodContent show = " + show);
-        }
-        mKeyguardStatusBar.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
-        mKeyguardBottomArea.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
-    }
-
 
     public void dozeTimeTick() {
         mLockIconViewController.dozeTimeTick();
