598add4f2fe0734c05e5a511a6cf580a2f616624
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-11 10:51:51 +0800

    core: Implement Oplus inspired vibration haptics
    * insipired from oplus haptics, based from aosp vibration effecs handling
    
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -2882,6 +2882,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
+            mController.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_TEXTURE_TICK));
         }
 
         @Override
