f05161488f8d243b807f22c4c08a99c6bc8a6f4a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-22 16:02:40 +0800

    SystemUI: Statusbar clock chip style [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -139,7 +139,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private final DumpManager mDumpManager;
     private final StatusBarWindowStateController mStatusBarWindowStateController;
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
-    private boolean mShowSBClockBg;
+    private int mShowSBClockBg;
 
     private List<String> mBlockedIcons = new ArrayList<>();
     private Map<Startable, Startable.State> mStartableStates = new ArrayMap<>();
@@ -392,7 +392,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         switch (key) {
             case STATUSBAR_CLOCK_CHIP:
                 mShowSBClockBg = 
-                        TunerService.parseIntegerSwitch(newValue, false);
+                        TunerService.parseInteger(newValue, 0);
                 updateStatusBarClock();
                 break;
             default:
@@ -401,8 +401,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     }
     
     private void updateStatusBarClock() {
-        if (mShowSBClockBg) {
-            mClockView.setBackgroundResource(R.drawable.sb_date_bg);
+        if (mShowSBClockBg != 0) {
+            String chipStyleUri = "sb_date_bg" + String.valueOf(mShowSBClockBg);
+            int resId = getContext().getResources().getIdentifier(chipStyleUri, "drawable", "com.android.systemui");
+            mClockView.setBackgroundResource(resId);
             mClockView.setPadding(10,2,10,2);
         } else {
             int clockPaddingStart = getResources().getDimensionPixelSize(
