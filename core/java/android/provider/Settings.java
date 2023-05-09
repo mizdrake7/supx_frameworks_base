7e80f0843fb516641e280f10c0f91ede3e69bd44
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-10 07:08:16 +0800

    SystemUI: Lockscreen text size settings [1/2]
    SuperDroidBond: adapt it for QPR2 Usage through ClockEventController
    
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Change-Id: I8bacfee46a9bde90848aa4a7b084c83b0d42cb0e
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11655,6 +11655,16 @@ public final class Settings {
          * @hide
          */
         public static final String KG_CUSTOM_CLOCK_TOP_MARGIN = "kg_custom_clock_top_margin";
+        
+        /**
+         * @hide
+         */
+        public static final String KG_LARGE_CLOCK_TEXT_SIZE = "kg_large_clock_text_size";
+
+        /**
+         * @hide
+         */
+        public static final String KG_SMALL_CLOCK_TEXT_SIZE = "kg_small_clock_text_size";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
