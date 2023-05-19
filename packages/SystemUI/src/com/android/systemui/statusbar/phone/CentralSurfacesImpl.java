ae8f868034183b15f6dc64cf4edc9032b00ce80b
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-19 15:38:01 +0800

    SystemUI: Disable touch events for dismiss all button if not visible
    Change-Id: I4b9fee2090806f7a9376ca9b6c11cff383980879
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1603,6 +1603,8 @@ public class CentralSurfacesImpl implements CoreStartable, PackageChangedListene
 	    int alpha = mNotificationPanelViewController.getExpandedFraction() < 0.84f ? 0 : Math.round(mNotificationPanelViewController.getExpandedFraction()  * 255.0f);
             mDismissAllButton.setAlpha(shouldHideDismissButton ? 0 : alpha);
             mDismissAllButton.getBackground().setAlpha(shouldHideDismissButton ? 0 : alpha);
+            mDismissAllButton.setClickable(!shouldHideDismissButton);
+            mDismissAllButton.setEnabled(!shouldHideDismissButton);
             mDismissAllButton.setVisibility(shouldHideDismissButton && isDismissButtonVisible ? View.GONE : View.VISIBLE);
     }
 
