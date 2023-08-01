PixelExperience/frameworks_base

FaceService: Allow our face unlock to be used on third-party apps
Change-Id: Ib9041442572885ceda3afbafd23f8e89226b0364

services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
@@ -25,6 +25,7 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.hardware.biometrics.BiometricManager;
+import android.hardware.biometrics.SensorProperties;
 import android.hardware.biometrics.BiometricsProtoEnums;
 import android.hardware.biometrics.IBiometricSensorReceiver;
 import android.hardware.biometrics.IBiometricService;
@@ -655,7 +656,7 @@ public class FaceService extends SystemService {
 
         private void addCustomProviders() {
             if (CustomFaceProvider.useCustomFaceUnlockService()) {
-                mServiceProviders.add(new CustomFaceProvider(getContext(), new FaceSensorPropertiesInternal(CustomFaceProvider.DEVICE_ID, 0, 1, new ArrayList(), 1, false, false, false), mLockoutResetDispatcher));
+                mServiceProviders.add(new CustomFaceProvider(getContext(), new FaceSensorPropertiesInternal(CustomFaceProvider.DEVICE_ID, SensorProperties.STRENGTH_STRONG, 1, new ArrayList(), 1, false, false, false), mLockoutResetDispatcher));
             }
         }
 

mizdrake7/supx_frameworks_base

services/core/java/com/android/server/biometrics/sensors/face/FaceService.java
@@ -25,6 +25,7 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.hardware.biometrics.BiometricManager;
+import android.hardware.biometrics.SensorProperties;
 import android.hardware.biometrics.BiometricsProtoEnums;
 import android.hardware.biometrics.IBiometricSensorReceiver;
 import android.hardware.biometrics.IBiometricService;
@@ -650,7 +651,7 @@ public class FaceService extends SystemService {
 
         private void addCustomProviders() {
             if (CustomFaceProvider.useCustomFaceUnlockService()) {
-                mServiceProviders.add(new CustomFaceProvider(getContext(), new FaceSensorPropertiesInternal(CustomFaceProvider.DEVICE_ID, 0, 1, new ArrayList(), 1, false, false, false), mLockoutResetDispatcher));
+                mServiceProviders.add(new CustomFaceProvider(getContext(), new FaceSensorPropertiesInternal(CustomFaceProvider.DEVICE_ID, SensorProperties.STRENGTH_WEAK, 1, new ArrayList(), 1, false, false, false), mLockoutResetDispatcher));
             }
         }
 