Neoteric-OS/android_frameworks_base

UsageStatsService: Fix the wrong content of the visible application UsageStats caused by the system time changed.
After the user adjusts the system time, the type of the first Event
reported is APP_ COMPONENT_ USED or USER_ INTERACTION, which may
cause problems with mLastTimeVisible and mTotalTimeVisible of
UsageStats of visible applications.

Change-Id: I69db518c7960da332eb068ae84b2aff435155ab9

services/usage/java/com/android/server/usage/UsageStatsService.java
@@ -995,7 +995,7 @@ public class UsageStatsService extends SystemService implements
      * convert it to a system wall time. System and real time snapshots are updated before
      * conversion.
      */
-    private void convertToSystemTimeLocked(Event event) {
+    private long getSystemTimeLocked(Event event) {
         final long actualSystemTime = System.currentTimeMillis();
         if (ENABLE_TIME_CHANGE_CORRECTION) {
             final long actualRealtime = SystemClock.elapsedRealtime();
@@ -1009,7 +1009,7 @@ public class UsageStatsService extends SystemService implements
                 mSystemTimeSnapshot = actualSystemTime;
             }
         }
-        event.mTimeStamp = Math.max(0, event.mTimeStamp - mRealTimeSnapshot) + mSystemTimeSnapshot;
+        return Math.max(0, event.mTimeStamp - mRealTimeSnapshot) + mSystemTimeSnapshot;
     }
 
     /**
@@ -1170,8 +1170,8 @@ public class UsageStatsService extends SystemService implements
                 case Event.USER_INTERACTION:
                     // Fall through
                 case Event.APP_COMPONENT_USED:
-                    convertToSystemTimeLocked(event);
-                    mLastTimeComponentUsedGlobal.put(event.mPackage, event.mTimeStamp);
+                    long systemTimeStamp = getSystemTimeLocked(event);
+                    mLastTimeComponentUsedGlobal.put(event.mPackage, systemTimeStamp);
                     break;
             }
 

mizdrake7/supx_frameworks_base

services/usage/java/com/android/server/usage/UsageStatsService.java
@@ -982,7 +982,7 @@ public class UsageStatsService extends SystemService implements
      * convert it to a system wall time. System and real time snapshots are updated before
      * conversion.
      */
-    private void convertToSystemTimeLocked(Event event) {
+    private long getAndCheckSystemTimeLocked(Event event) {
         final long actualSystemTime = System.currentTimeMillis();
         if (ENABLE_TIME_CHANGE_CORRECTION) {
             final long actualRealtime = SystemClock.elapsedRealtime();
@@ -996,7 +996,7 @@ public class UsageStatsService extends SystemService implements
                 mSystemTimeSnapshot = actualSystemTime;
             }
         }
-        event.mTimeStamp = Math.max(0, event.mTimeStamp - mRealTimeSnapshot) + mSystemTimeSnapshot;
+        return Math.max(0, event.mTimeStamp - mRealTimeSnapshot) + mSystemTimeSnapshot;
     }
 
     /**
@@ -1157,8 +1157,8 @@ public class UsageStatsService extends SystemService implements
                 case Event.USER_INTERACTION:
                     // Fall through
                 case Event.APP_COMPONENT_USED:
-                    convertToSystemTimeLocked(event);
-                    mLastTimeComponentUsedGlobal.put(event.mPackage, event.mTimeStamp);
+                    long systemTimeStamp = getAndCheckSystemTimeLocked(event);
+                    mLastTimeComponentUsedGlobal.put(event.mPackage, systemTimeStamp);
                     break;
             }
 