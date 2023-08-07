AICP/frameworks_base

NotificationLights: add reverse animation direction
When notifications are repeated more than once, every
other notification light starts where the previous one
ends.

Change-Id: I56ec562badcc7dc92e63bd212974763557b83182

packages/SystemUI/src/com/android/systemui/aicp/NotificationLightsView.java
@@ -99,6 +99,9 @@ public class NotificationLightsView extends RelativeLayout {
         int repeat = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_AMBIENT_LIGHT_REPEAT_COUNT, 0,
                 UserHandle.USER_CURRENT);
+        boolean directionIsRestart = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_REPEAT_DIRECTION, 0,
+                UserHandle.USER_CURRENT) != 1;
         int colorMode = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_AMBIENT_LIGHT_COLOR_MODE, 1,
                 UserHandle.USER_CURRENT);
@@ -133,7 +136,7 @@ public class NotificationLightsView extends RelativeLayout {
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
         mLightAnimator.setDuration(duration);
         mLightAnimator.setRepeatCount(repeat);
-        mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
+        mLightAnimator.setRepeatMode(directionIsRestart ? ValueAnimator.RESTART : ValueAnimator.REVERSE);
         mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
                 Log.e("NotificationLightsView", "onAnimationUpdate");

mizdrake7/supx_frameworks_base

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationLightsView.java
@@ -126,6 +126,9 @@ public class NotificationLightsView extends RelativeLayout {
         int repeats = Settings.System.getIntForUser(resolver,
                 Settings.System.NOTIFICATION_PULSE_REPEATS, 0,
                 UserHandle.USER_CURRENT);
+        boolean directionIsRestart = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_LIGHT_REPEAT_DIRECTION, 0,
+                UserHandle.USER_CURRENT) != 1;
 
         ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
         ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
@@ -135,7 +138,7 @@ public class NotificationLightsView extends RelativeLayout {
         mLightAnimator.setDuration(duration);
         mLightAnimator.setRepeatCount(repeats == 0 ?
                 ValueAnimator.INFINITE : repeats - 1);
-        mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
+        mLightAnimator.setRepeatMode(directionIsRestart ? ValueAnimator.RESTART : ValueAnimator.REVERSE);
         if (repeats != 0) {
             mLightAnimator.addListener(new AnimatorListener() {
                 @Override