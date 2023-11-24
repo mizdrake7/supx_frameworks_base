7fdee03e64ec3b772cee0f37716f552da2372aab
Giovanni Ricca <giovanniricca@proton.me>
2023-11-24 23:42:40 +0000

    Why did the sailfish start a band?
    Because it had a great sense of scale and wanted to make some fin-tastic music!
    
    Change-Id: I5ec2bb94b5ef60ae69940d7452d8650f4f549189

--- core/java/com/android/internal/util/superior/PixelPropsUtils.java
+++ core/java/com/android/internal/util/superior/PixelPropsUtils.java
@@ -205,12 +205,11 @@ public class PixelPropsUtils {
                 if (processName.equals("com.google.android.gms.unstable")) {
                     sIsGms = true;
                     // Alter model name and fingerprint to Redmi Go to avoid hardware attestation enforcement
-                // Alter build parameters to Nexus 6P for avoiding hardware attestation enforcement
-                setPropValue("DEVICE", "bullhead");
-                setPropValue("FINGERPRINT", "google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys");
-                setPropValue("MODEL", "Nexus 5X");
-                setPropValue("PRODUCT", "bullhead");
-                setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N);
+                setPropValue("DEVICE", "sailfish");
+        	setPropValue("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM2.171019.029/4657601:user/release-keys");
+        	setPropValue("MODEL", "Pixel");
+        	setPropValue("PRODUCT", "sailfish");
+        	setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.O_MR1);
                 }
                 return;
             }
